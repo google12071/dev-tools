4fd19e4adee343869583c4f04ec05ebb7d685e0a
lifeiqiang <lifeiqiang@qbb6.com>
2019-10-11 16:47:52 +0800

    分布式锁

---
+++ microservice/src/main/java/com/learn/ms/zk/ZkDistributeAdvanceLock.java
@@ -0,0 +1,174 @@
+package com.learn.ms.zk;
+
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.exception.ZkNodeExistsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 优化后的分布式锁:
+ *   利用临时顺序节点来实现分布式锁
+ *   获取锁：取顺序号（类似银行排队拿号），判断自己的序号是否最小，若是则获得锁，否则注册前一个兄弟节点watcher，则阻塞等待
+ *   释放锁：删除自己创建的临时顺序节点
+ *
+ */
+public class ZkDistributeAdvanceLock implements Lock {
+    private static final Logger logger = LoggerFactory.getLogger(ZkDistributeAdvanceLock.class);
+
+    private final CountDownLatch downLatch = new CountDownLatch(1);
+
+
+    private String path;
+
+    private ZkClient zkClient;
+
+    /**
+     * 当前路径
+     */
+    private ThreadLocal<String> currentPath = new ThreadLocal<>();
+
+    /**
+     * 前一个兄弟节点路径
+     */
+    private ThreadLocal<String> beforePath = new ThreadLocal<>();
+
+    private ThreadLocal<Integer> reenterCount = ThreadLocal.withInitial(() -> 0);
+
+    public ZkDistributeAdvanceLock(String path) {
+        if (path == null || path.trim().equals("")) {
+            throw new IllegalArgumentException("路径为空!");
+        }
+
+        this.path = path;
+
+        zkClient = new ZkClient("localhost:2181");
+
+        zkClient.setZkSerializer(new CustomSerializer());
+
+        try {
+            if (!zkClient.exists(path)) {
+                zkClient.createPersistent(path, true);
+            }
+        } catch (ZkNodeExistsException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void lock() {
+        //尝试获取分布式锁，若获取不到则等待，监听到上一个兄弟节点删除后，再次尝试获取锁
+        if (!tryLock()) {
+            //阻塞等待
+            waitForLock();
+            //再次竞争分布式锁
+            lock();
+        }
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        logger.info(Thread.currentThread().getName() + "尝试获取分布式锁...");
+
+        //先创建临时顺序节点
+        if (currentPath.get() == null || !zkClient.exists(currentPath.get())) {
+            String node = zkClient.createEphemeralSequential(path + "/", "locked");
+            currentPath.set(node);
+            reenterCount.set(0);
+        }
+
+        //获取所有节点
+        List<String> childList = zkClient.getChildren(path);
+
+        //所有子节点排序
+        Collections.sort(childList);
+
+        //若是最小节点，可获得锁
+        if (currentPath.get().equals(path + "/" + childList.get(0))) {
+            //重入计数+1
+            reenterCount.set(reenterCount.get() + 1);
+            logger.info(Thread.currentThread().getName() + "获得分布式锁...");
+            return true;
+
+        } else {
+            //获取上一个兄弟节点
+            int currentIndex = childList.indexOf(currentPath.get().substring(path.length() + 1));
+            String node = path + "/" + childList.get(currentIndex - 1);
+            beforePath.set(node);
+            logger.info(Thread.currentThread().getName() + "获取分布式锁失败...");
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+
+    @Override
+    public void unlock() {
+        logger.info(Thread.currentThread() + "释放当前分布式锁");
+        if (reenterCount.get() > 1) {
+            //重入次数-1，释放锁
+            reenterCount.set(reenterCount.get() - 1);
+            return;
+        }
+
+        //删除节点
+        if (currentPath.get() != null) {
+            zkClient.delete(currentPath.get());
+            currentPath.set(null);
+            reenterCount.set(0);
+        }
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+
+    private void waitForLock() {
+        //监听上一个兄弟节点数据变更
+        IZkDataListener dataListener = new IZkDataListener() {
+            @Override
+            public void handleDataChange(String s, Object o) throws Exception {
+                logger.info(Thread.currentThread().getName() + "监听到节点删除");
+                downLatch.countDown();
+            }
+
+            @Override
+            public void handleDataDeleted(String s) throws Exception {
+                logger.info(Thread.currentThread().getName() + "监听到节点变更");
+            }
+        };
+
+        zkClient.subscribeDataChanges(beforePath.get(), dataListener);
+
+        //阻塞自己
+        if (zkClient.exists(beforePath.get())) {
+            logger.info(Thread.currentThread().getName() + "分布式锁没有抢到，进入阻塞状态");
+            try {
+                downLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //取消监听
+        zkClient.unsubscribeDataChanges(beforePath.get(), dataListener);
+    }
+}
---
+++ microservice/src/main/java/com/learn/ms/zk/ZkDistributeLock.java
@@ -0,0 +1,111 @@
+package com.learn.ms.zk;
+
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.exception.ZkNodeExistsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 基于zk实现的分布式锁
+ */
+public class ZkDistributeLock implements Lock {
+
+    private static final Logger logger = LoggerFactory.getLogger(ZkDistributeLock.class);
+
+    private final CountDownLatch downLatch = new CountDownLatch(1);
+
+    private String path;
+
+    private ZkClient zkClient;
+
+    public ZkDistributeLock(String path) {
+        if (path == null || path.trim().equals("")) {
+            throw new IllegalArgumentException("路径为空!");
+        }
+        this.path = path;
+
+        zkClient = new ZkClient("localhost:2181");
+
+        zkClient.setZkSerializer(new CustomSerializer());
+    }
+
+    @Override
+    public void lock() {
+        //尝试加锁失败，则阻塞等待
+        if (!tryLock()) {
+            //没有获得锁，当前线程阻塞
+            waitForLock();
+            //从等待中被唤醒，再次尝试加锁
+            lock();
+        }
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        try {
+            zkClient.createEphemeral(path);
+        } catch (ZkNodeExistsException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+        zkClient.delete(path);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+
+    private void waitForLock() {
+
+        //设置节点监听
+        IZkDataListener dataListener = new IZkDataListener() {
+            @Override
+            public void handleDataChange(String s, Object o) throws Exception {
+                logger.info(s + ",监听节点数据发生变动,data:" + o);
+            }
+
+            @Override
+            public void handleDataDeleted(String s) throws Exception {
+                logger.info("监听节点被删除，可重新尝试加锁");
+
+                //唤醒阻塞线程
+                downLatch.countDown();
+            }
+        };
+        zkClient.subscribeDataChanges(path, dataListener);
+
+        //阻塞当前线程
+        try {
+            if (zkClient.exists(path)) {
+                downLatch.await();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //取消监听
+        zkClient.unsubscribeDataChanges(path, dataListener);
+    }
+}
