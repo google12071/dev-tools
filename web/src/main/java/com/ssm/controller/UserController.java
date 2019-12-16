664380963fae9b4088f769dd21ace9260db0934b
lifeiqiang <lifeiqiang@qbb6.com>
2019-12-16 19:58:45 +0800

    工程结构重构

--- web/src/main/java/com/ssm/controller/UserController.java
+++ web/src/main/java/com/ssm/controller/UserController.java
@@ -2,21 +2,20 @@ package com.ssm.controller;
 
 import com.ssm.model.User;
 import com.ssm.service.UserService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
 
     @Resource
     private UserService userService;
 
-    @ResponseBody
+
     @RequestMapping(value = "/user/list", method = RequestMethod.GET)
     public List<User> getAllUser() {
         return userService.getAllUser();
