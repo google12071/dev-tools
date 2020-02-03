129216c7e24f61ec049c4d1b34cf1a3bab26be99
lifeiqiang <Fqli12071@163.com>
2020-02-03 16:53:38 +0800

    Guava工具类

---
+++ interview/src/main/java/com/learn/javasrc/util/ConcurrentTest.java
@@ -0,0 +1,65 @@
+package com.learn.javasrc.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 并发集合类(阻塞队列应用)
+ *
+ * @author lfq
+ */
+@Slf4j
+public class ConcurrentTest {
+    private static final LinkedBlockingQueue<Integer> blockingQueue = new LinkedBlockingQueue<>();
+
+    public static class Producer implements Runnable {
+        private Integer value;
+
+        public Producer(Integer value) {
+            this.value = value;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    log.info(Thread.currentThread().getName() + ",put value:" + value);
+                    blockingQueue.put(value);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class Consumer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Integer value = blockingQueue.take();
+                    log.info(Thread.currentThread().getName() + ",get value:" + value);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Thread[] producerThreads = new Thread[5];
+        Thread[] consumerThreads = new Thread[10];
+
+        for (int i = 0; i < producerThreads.length; i++) {
+            producerThreads[i] = new Thread(new Producer(i));
+            producerThreads[i].start();
+        }
+
+        for (int i = 0; i < consumerThreads.length; i++) {
+            consumerThreads[i] = new Thread(new Consumer());
+            consumerThreads[i].start();
+        }
+    }
+}
---
+++ interview/src/main/java/com/learn/javasrc/util/QueueTest.java
@@ -0,0 +1,12 @@
+package com.learn.javasrc.util;
+
+/**
+ * 队列数据结构
+ *
+ * @author lfq
+ */
+public class QueueTest {
+    public static void main(String[] args) {
+
+    }
+}
