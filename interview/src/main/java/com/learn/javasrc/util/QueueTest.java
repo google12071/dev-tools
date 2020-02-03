5164cdc947171ccca5b0e4402e68f34dfeac40ff
lifeiqiang <Fqli12071@163.com>
2020-02-03 18:48:56 +0800

    SynchronousQueue队列测试

--- interview/src/main/java/com/learn/javasrc/util/QueueTest.java
+++ interview/src/main/java/com/learn/javasrc/util/QueueTest.java
@@ -1,12 +1,44 @@
 package com.learn.javasrc.util;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.SynchronousQueue;
+
 /**
  * 队列数据结构
  *
  * @author lfq
  */
+@Slf4j
 public class QueueTest {
-    public static void main(String[] args) {
+    private static final SynchronousQueue<Integer> queue = new SynchronousQueue<>(true);
 
+    public static class TakeThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Integer value = queue.take();
+                    log.info(Thread.currentThread().getName() + ",take value:" + value);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Thread thread = new TakeThread();
+            thread.setName("take线程");
+            thread.start();
+            int i = 0;
+            while (i<1000) {
+                queue.put(i++);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
